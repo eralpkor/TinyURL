6365415578a925806a44fbca5529b77a79a19ae1
eralpkor <eralp.ny@gmail.com>
2020-07-11 16:42:27 -0400

    Add dotenv

--- src/base-route/base-router.js
+++ src/base-route/base-router.js
@@ -1,6 +1,9 @@
+
 const router = require('express').Router();
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost/url-shortner';
+// const mongoURI = 'mongodb://localhost/url-shortner';
+const mongoURI = 'mongodb+srv://admin:' + process.env.PASSWORD + '@cluster0.m2oo8.mongodb.net/tinyurl?retryWrites=true&w=majority';
+
 const connectOptions = {
   keepAlive: true,
   // reconnectTries: Number.MAX_VALUE,
@@ -11,8 +14,11 @@ const connectOptions = {
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoURI, connectOptions, (err, db) => {
-  if (err) console.log('Error: ', err);
+  if (err) {
+    console.log('Error: ', err);
+  } else {
   console.log('Connected to mongodb...');
+  }
 });
 
 module.exports = router;
